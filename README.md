db0737a70ac3b47945556324e0f29925c438510d
Liam Wiltshire <liam@w.iltshi.re>
2022-12-21 13:49:57 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# tech-task
+# Tebex Tech Test
+
+## Task
+
+### Part 1
+Refactor the LookupController in this codebase. In particular, consider how Composition, Inheritance and Contracts can refactor this code in a clean, maintainable way.
+Ensure that the final code follows PSR-12 standards (hint, some of it currently doesn't), and structure the code in a way that shows your knowledge of:
+
+- SOLID principles
+- OOP
+- Contracts (Interfaces)
+- Dependency Injection (In Laravel this also includes use of the Service Container)
+
+
+### Part 2
+Write some unit or feature tests for the code you've written. You do not need to have complete test coverage we just want to see that you have some experience in writing automated tests!
+
+### Bonus Points
+
+- Due to rate limits enforced by the underlying services, consider how data can be cached or persisted so that we're not having to call the underlying service every time
+- Implement some 'defensive programming' (consider how and why the application might fail and implement appropriate precautions)
+- Consider how error/fail states should be communicated back to the user
+
+## Example Requests and expected Results:
+(Note: This assumes the code is running on http://localhost:8000) - e.g. having been started using the built-in php server:
+
+`php -S localhost:8000 -t public`
+
+http://localhost:8000/lookup?type=xbl&username=tebex
+```json
+{"username":"Tebex","id":"2533274844413377","avatar":"https:\/\/avatar-ssl.xboxlive.com\/avatar\/2533274844413377\/avatarpic-l.png"}
+```
+
+http://localhost:8000/lookup?type=xbl&id=2533274884045330
+```json
+{"username":"d34dmanwalkin","id":"2533274884045330","avatar":"https:\/\/avatar-ssl.xboxlive.com\/avatar\/2533274884045330\/avatarpic-l.png"}
+```
+
+http://localhost:8000/lookup?type=steam&username=test
+Should return an error "Steam only supports IDs"
+
+http://localhost:8000/lookup?type=steam&id=76561198806141009
+```json
+{"username":"Tebex","id":"76561198806141009","avatar":"https:\/\/steamcdn-a.akamaihd.net\/steamcommunity\/public\/images\/avatars\/c8\/c86f94b0515600e8f6ff869d13394e05cfa0cd6a.jpg"}
+```
+
+http://localhost:8000/lookup?type=minecraft&id=d8d5a9237b2043d8883b1150148d6955
+```json
+{"username":"Test","id":"d8d5a9237b2043d8883b1150148d6955","avatar":"https:\/\/crafatar.com\/avatarsd8d5a9237b2043d8883b1150148d6955"}
+```
+
+http://localhost:8000/lookup?type=minecraft&username=Notch
+```json
+{"username":"Notch","id":"069a79f444e94726a5befca90e38aaf5","avatar":"https:\/\/crafatar.com\/avatars069a79f444e94726a5befca90e38aaf5"}
+```
+
---
+++ app/Http/Controllers/LookupController.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+/**
+ * Class LookupController
+ *
+ * @package App\Http\Controllers
+ */
+class LookupController extends Controller
+{
+    public function lookup(Request $request) {
+        if ($request->get('type') == 'minecraft') {
+            if ($request->get('username')) {
+                $username = $request->get('username');
+                $userId = false;
+            }
+            if ($request->get('id')){
+                $username=false;
+                $userId = $request->get('id');
+            }
+
+            if ($username) {
+                $guzzle = new Client();
+                $response = $guzzle->get(
+                    "https://api.mojang.com/users/profiles/minecraft/{$username}"
+                );
+
+                $match = json_decode($response->getBody()->getContents());
+
+                return [
+                    'username' => $match->name,
+                    'id' => $match->id,
+                    'avatar' => "https://crafatar.com/avatars" . $match->id
+                ];
+            }
+
+            if ($userId)
+            {
+                $guzzle = new Client();
+                $response = $guzzle->get(
+                    "https://sessionserver.mojang.com/session/minecraft/profile/{$userId}"
+                );
+
+                $match = json_decode($response->getBody()->getContents());
+                return [
+                    'username' => $match->name,
+                    'id' => $match->id,
+                    'avatar' => "https://crafatar.com/avatars" . $match->id
+                ];
+            }
+        } elseif ($request->get('type')=='steam') {
+            if ($request->get("username")) {
+                die("Steam only supports IDs");
+            } else {
+                $id = $request->get("id");
+                $guzzle = new Client();
+                $url = "https://ident.tebex.io/usernameservices/4/username/{$id}";
+
+                $match = json_decode($guzzle->get($url)->getBody()->getContents());
+
+                return [
+                    'username' => $match->username,
+                    'id' => $match->id,
+                    'avatar' => $match->meta->avatar
+                ];
+            }
+
+        }elseif($request->get('type') === 'xbl'){
+            if ($request->get("username")) {
+                $guzzle = new Client();
+                $response = $guzzle->get("https://ident.tebex.io/usernameservices/3/username/" . $request->get("username") . "?type=username");
+                $profile = json_decode($response->getBody()->getContents());
+
+                return [
+                    'username' => $profile->username,
+                    'id' => $profile->id,
+                    'avatar' => $profile->meta->avatar
+                ];
+            }
+
+            if ($request->get("id")) {
+                $id = $request->get("id");
+                $guzzle = new Client();
+                $response = $guzzle->get("https://ident.tebex.io/usernameservices/3/username/" . $id);
+                $profile = json_decode($response->getBody()->getContents());
+
+                return [
+                    'username' => $profile->username,
+                    'id' => $profile->id,
+                    'avatar' => $profile->meta->avatar
+                ];
+            }
+        }
+        //We can't handle this - maybe provide feedback?
+        die();
+    }
+}
